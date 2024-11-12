7569dd104efcd88f96c9054a016751495a2c3655
adambor <adamborteam@gmail.com>
2024-11-12 17:08:12 +0100

    sdk-base: add typeguards for token types

--- crosslightning-sdk-base/src/swaps/ISwap.ts
+++ crosslightning-sdk-base/src/swaps/ISwap.ts
@@ -46,13 +46,30 @@ export type BtcToken<L = boolean> = {
     lightning: L
 };
 
+export function isBtcToken(obj: any): obj is BtcToken {
+    return typeof(obj)==="object" &&
+        obj.chain==="BTC" &&
+        typeof(obj.lightning)==="boolean";
+}
+
 export type SCToken<ChainIdentifier extends string = string> = {
     chain: "SC",
     chainId: ChainIdentifier,
     address: string
 }
 
+export function isSCToken(obj: any): obj is SCToken {
+    return typeof(obj)==="object" &&
+        obj.chain==="SC" &&
+        typeof(obj.chainId)==="string" &&
+        typeof(obj.address)==="string";
+}
+
 export type Token<ChainIdentifier extends string = string> = BtcToken | SCToken<ChainIdentifier>;
+
+export function isToken(obj: any): obj is Token {
+    return isBtcToken(obj) || isSCToken(obj);
+}
 
 export abstract class ISwap<
     T extends ChainType = ChainType,
